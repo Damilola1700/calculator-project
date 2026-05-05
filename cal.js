d6ef9bcd12e56f660a36027e563bd7a41941aa9e
loveth <lovethdamilola206@gmail.com>
2026-05-05 12:30:23 +0100

    calculator

--- cal.js
+++ cal.js
@@ -1,14 +1,19 @@
 let display = document. getElementById('output')
 
 
+
 function addinput(add){
     display.value += add;
 }
 
-function equalstoo(){
+function total(){
     display.value = eval(display.value)
 }
 
 function clearInput(){
     display.value = "";
+}
+
+function clearLast(){
+    display.value = display.value.slice(0 ,-1);
 }
